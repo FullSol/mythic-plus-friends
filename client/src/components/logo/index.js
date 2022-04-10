3b51aa601d6442bbb585559a58c579f91a41098e
FullSol <ceraines@student.fullsail.edu>
2022-04-10 12:09:48 -0400

    created logo component

---
+++ client/src/components/logo/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+
+function Logo() {
+  return (
+    <Avatar
+      alt="mythic plus friends logo"
+      src="/images/Finished_MythicLogos-1.png"
+    />
+  );
+}
