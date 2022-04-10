819ff461ca88926d7ebc92afd6ba14a3797f1e2b
FullSol <ceraines@student.fullsail.edu>
2022-04-10 11:34:10 -0400

    added packages and configurations

--- client/src/components/social/index.js
+++ client/src/components/social/index.js
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import Avatar from '@mui/material/Avatar';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import Avatar from "@mui/material/Avatar";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PropTypes from "prop-types";
 
 function getIcon(iconType) {
   // Switch case to return the correct icon
   switch (iconType) {
-    case 'facebook':
-      return <FacebookIcon />;
-    case 'twitter':
+    case "facebook":
+      return <FacebookIcon sx={{ fontSize: "60px", opacity: ".56" }} />;
+    case "twitter":
       return <TwitterIcon />;
-    case 'youtube':
+    case "youtube":
       return <YouTubeIcon />;
-    case 'instagram':
+    case "instagram":
       return <InstagramIcon />;
     default:
       return null;
   }
 }
 
-const Social = (props) => {
+function Social(props) {
   const { type, link } = props;
 
-  const [icon, setIcon] = useState('');
+  const [icon, setIcon] = useState("");
 
   useEffect(() => {
     // Set string lower case and get the icon
@@ -33,19 +33,22 @@ const Social = (props) => {
   }, [type]);
 
   // Don't return if there is nothing to return
-  if (icon === '' || icon === null) {
+  if (icon === "" || icon === null) {
     return null;
   }
 
   // Return the Avatar and the link
   return (
-    <>
-      <a href={link}>
-        <Avatar>{icon}</Avatar>
-      </a>
-    </>
+    <a href={link}>
+      <Avatar
+        sx={{ height: 65, width: 65, backgroundColor: "rgba(255,255,255,0)" }}
+        component="p"
+      >
+        {icon}
+      </Avatar>
+    </a>
   );
-};
+}
 
 Social.propTypes = {
   type: PropTypes.string.isRequired,
@@ -53,7 +56,7 @@ Social.propTypes = {
 };
 
 Social.defaultProps = {
-  link: '#',
+  link: "#",
 };
 
 export default Social;
